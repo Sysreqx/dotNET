1cac4cb52e0cbdcc8ed8262302c4286823789808
sysreqx <sysreqx@gmail.com>
2018-07-01 20:26:43 +0600

    books

---
+++ proC#7/chapter3.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+// 10
+namespace Test
+{
+    class Program
+    {
+
+        #region MyRegion
+        static void NewingDataTypes()
+        {
+            Console.WriteLine("=> Using new to create variables:");
+            bool b = new bool(); // Set to false.
+            int i = new int(); // Set to 0.
+            double d = new double(); // Set to 0.
+            DateTime dt = new DateTime(); // Set to 1/1/0001 12:00:00 AM
+            Console.WriteLine("{0}, {1}, {2}, {3}", b, i, d, dt);
+            Console.WriteLine();
+        }
+        static void ObjectFunctionality()
+        {
+            Console.WriteLine("=> System.Object Functionality:");
+            // A C# int is really a shorthand for System.Int32,
+            // which inherits the following members from System.Object.
+            Console.WriteLine("12.GetHashCode() = {0}", 12.GetHashCode());
+            Console.WriteLine("12.Equals(23) = {0}", 12.Equals(23));
+            Console.WriteLine("12.ToString() = {0}", 12.ToString());
+            Console.WriteLine("12.GetType() = {0}", 12.GetType());
+            Console.WriteLine();
+        }
+        static void DataTypeFunctionality()
+        {
+            Console.WriteLine("bool.FalseString: {0}", bool.FalseString);
+            Console.WriteLine("bool.TrueString: {0}", bool.TrueString);
+        }
+        static void ParseFromStringsWithTryParse()
+        {
+            Console.WriteLine("=> Data type parsing with TryParse:");
+            if (bool.TryParse("True", out bool b)) 
+            {
+                Console.WriteLine("Value of b: {0}", b);
+            }
+            string value = "Hello";
+            if (double.TryParse(value, out double d))
+            {
+                Console.WriteLine("Value of d: {0}", d);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert the input ({0}) to a double", value);
+            }
+            Console.WriteLine();
+        }
+        static void CharFunctionality()
+        {
+            Console.WriteLine("=> char type Functionality:");
+            char myChar = 'a';
+            Console.WriteLine("char.IsDigit('a'): {0}", char.IsDigit(myChar));
+            Console.WriteLine("char.IsLetter('a'): {0}", char.IsLetter(myChar));
+            Console.WriteLine("char.IsWhiteSpace('Hello There', 5): {0}",
+            char.IsWhiteSpace("Hello There", 5));
+            Console.WriteLine("char.IsWhiteSpace('Hello There', 6): {0}",
+            char.IsWhiteSpace("Hello There", 6));
+
+            Console.WriteLine(char.IsWhiteSpace("q ", 0));
+            Console.WriteLine("char.IsPunctuation('?'): {0}",
+            char.IsPunctuation('?'));
+            Console.WriteLine();
+        }
+        static void UseDatesAndTimes()
+        {
+            Console.WriteLine("=> Dates and Times:");
+            // This constructor takes (year, month, day).
+            DateTime dt = new DateTime(2015, 10, 17);
+            // What day of the month is this?
+            Console.WriteLine("The day of {0} is {1}", dt.Date, dt.DayOfWeek);
+            // Month is now December.
+            dt = dt.AddMonths(2);
+            Console.WriteLine("Daylight savings: {0}", dt.IsDaylightSavingTime());
+            // This constructor takes (hours, minutes, seconds).
+            TimeSpan ts = new TimeSpan(4, 30, 0);
+            Console.WriteLine(ts);
+            // Subtract 15 minutes from the current TimeSpan and
+            // print the result.
+            Console.WriteLine(ts.Subtract(new TimeSpan(0, 55, 0)));
+        }
+        static void UseBigInteger()
+        {
+            Console.WriteLine("=> Use BigInteger:");
+            BigInteger biggy =
+            BigInteger.Parse("999999999999999999999999999999123462346234623999239999999999988888882456245688888835648456845888888458456888888888234621346234888888823423488888888889999999999999993456244583299999999999999999999999999999999999999912346234623462399923999999999998888888245624568888883564845684588888845845688888888823462134623488888882342348888888888999999999999999345624458329999999999999999999999999999999999999991234623462346239992399999999999888888824562456888888356484568458888884584568888888882346213462348888888234234888888888899999999999999934562445832999999999");
+            Console.WriteLine("Value of biggy is {0}", biggy);
+            Console.WriteLine("Is biggy an even value?: {0}", biggy.IsEven);
+            Console.WriteLine("Is biggy a power of two?: {0}", biggy.IsPowerOfTwo);
+            BigInteger reallyBig = BigInteger.Multiply(biggy,
+            BigInteger.Parse("88888882456245688888835648456845888888458456888888888234621346234888888823423488888888888888888245624568888883564845684588888845845688888888823462134623488888882342348888888888888888824562456888888356484568458888884584568888888882346213462348888888234234888888888888888882456245688888835648456845888888458456888888888234621346234888888823423488888888888888888245624568888883564845684588888845845688888888823462134623488888882342348888888888"));
+            Console.WriteLine("Value of reallyBig is {0}", reallyBig);
+        }
+        static void DigitSeparators()
+        {
+            Console.WriteLine("=> Use Digit Separators:");
+            Console.Write("Integer:");
+            Console.WriteLine(123_456);
+            Console.Write("Long:");
+            Console.WriteLine(123_456_789L);
+            Console.Write("Float:");
+            Console.WriteLine(123_456.1234F);
+            Console.Write("Double:");
+            Console.WriteLine(123_456.12);
+            Console.Write("Decimal:");
+            Console.WriteLine(123_456.12M);
+        }
+        private static void BinaryLiterals()
+        {
+            Console.WriteLine("=> Use Binary Literals:");
+            Console.WriteLine("Sixteen: {0}", 0b0001_0001);
+            Console.WriteLine("Thirty Two: {0}", 0b0010_0000);
+            Console.WriteLine("Sixty Four: {0}", 0b0100_0000);
+        }
+        static void BasicStringFunctionality()
+        {
+            Console.WriteLine("=> Basic String functionality:");
+            string firstName = "Freddy";
+            Console.WriteLine("Value of firstName: {0}", firstName);
+            Console.WriteLine("firstName has {0} characters.", firstName.Length);
+            Console.WriteLine("firstName in uppercase: {0}", firstName.ToUpper());
+            Console.WriteLine("firstName in lowercase: {0}", firstName.ToLower());
+            Console.WriteLine("firstName contains the letter y?: {0}",
+            firstName.Contains("y"));
+            Console.WriteLine("firstName after replace: {0}", firstName.Replace("dy", ""));
+            Console.WriteLine();
+        }
+        static void StringConcatenation()
+        {
+            Console.WriteLine("=> String concatenation:");
+            string s1 = "Programming the ";
+            string s2 = "PsychoDrill (PTP)";
+            string s3 = s1 + s2;
+            Console.WriteLine(s3);
+            Console.WriteLine();
+        }
+        static void EscapeChars()
+        {
+            Console.WriteLine("=> Escape characters:\a");
+            string strWithTabs = "Model\tColor\tSpeed\tPet Name\a ";
+            Console.WriteLine(strWithTabs);
+            Console.WriteLine("Everyone loves \"Hello World\"\a ");
+            Console.WriteLine("C:\\MyApp\\bin\\Debug\a ");
+            // Adds a total of 4 blank lines (then beep again!).
+            Console.WriteLine("All finished.\n\n\n\a ");
+            Console.WriteLine();
+        }
+        static void StringEquality()
+        {
+            Console.WriteLine("=> String equality:");
+            string s1 = "Hello!";
+            string s2 = "Yo!";
+            Console.WriteLine("s1 = {0}", s1);
+            Console.WriteLine("s2 = {0}", s2);
+            Console.WriteLine();
+            // Test these strings for equality.
+            Console.WriteLine("s1 == s2: {0}", s1 == s2);
+            Console.WriteLine("s1 == Hello!: {0}", s1 == "Hello!");
+            Console.WriteLine("s1 == HELLO!: {0}", s1 == "HELLO!");
+            Console.WriteLine("s1 == hello!: {0}", s1 == "hello!");
+            Console.WriteLine("s1.Equals(s2): {0}", s1.Equals(s2));
+            Console.WriteLine("Yo.Equals(s2): {0}", "Yo!".Equals(s2));
+            Console.WriteLine();
+        }
+        static void StringEqualitySpecifyingCompareRules()
+        { Console.WriteLine("=> String equality (Case Insensitive:"); string s1 = "Hello!"; string s2 = "HELLO!"; Console.WriteLine("s1 = {0}", s1); Console.WriteLine("s2 = {0}", s2); Console.WriteLine();  // Check the results of changing the default compare rules.
+            Console.WriteLine("Default rules: s1={0},s2={1}s1.Equals(s2): {2}", s1, s2, s1.Equals(s2));  Console.WriteLine("Ignore case: s1.Equals(s2, StringComparison.OrdinalIgnoreCase): {0}",  s1.Equals(s2, StringComparison.OrdinalIgnoreCase));  Console.WriteLine("Ignore case, Invarariant Culture: s1.Equals(s2, StringComparison. InvariantCultureIgnoreCase): {0}",  s1.Equals(s2, StringComparison.InvariantCultureIgnoreCase));  Console.WriteLine();  Console.WriteLine("Default rules: s1={0},s2={1} s1.IndexOf(\"E\"): {2}", s1, s2, s1.IndexOf("E"));  Console.WriteLine("Ignore case: s1.IndexOf(\"E\", StringComparison.OrdinalIgnoreCase): {0}", s1.IndexOf("E",  StringComparison.OrdinalIgnoreCase));  Console.WriteLine("Ignore case, Invarariant Culture: s1.IndexOf(\"E\", StringComparison. InvariantCultureIgnoreCase): {0}",  s1.IndexOf("E", StringComparison.InvariantCultureIgnoreCase));  Console.WriteLine(); }
+
+        static void StringsAreImmutable()
+        {
+            // Set initial string value.
+            string s1 = "This is my string.";
+            Console.WriteLine("s1 = {0}", s1);
+            // Uppercase s1?
+            string upperString = s1.ToUpper();
+            Console.WriteLine("upperString = {0}", upperString);
+            // Nope! s1 is in the same format!
+            Console.WriteLine("s1 = {0}", s1);
+        }
+        static void StringsAreImmutable2()
+        {
+            string s2 = "My other string";
+            s2 = "New string value";
+        }
+        static void FunWithStringBuilder()
+        {
+            Console.WriteLine("=> Using the StringBuilder:");
+            StringBuilder sb = new StringBuilder("**** Fantastic Games ****");
+            sb.Append("\n");
+            sb.AppendLine("Half Life");
+            sb.AppendLine("Morrowind");
+            sb.AppendLine("Deus Ex" + "2");
+            sb.AppendLine("System Shock");
+            Console.WriteLine(sb.ToString());
+            sb.Replace("2", " Invisible War");
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine("sb has {0} chars.", sb.Length);
+            Console.WriteLine();
+        }
+        static void StringInterpolation()
+        {
+            // Some local variables we will plug into our larger string
+            int age = 4;
+            string name = "Soren";
+            // Using curly bracket syntax.
+            string greeting = string.Format("Hello {0} you are {1} years old.", name, age);
+            // Using string interpolation
+            string greeting2 = $"Hello {name} you are {age} years old.";
+            greeting = string.Format("Hello {0} you are {1} years old.", name.ToUpper(), age);
+            greeting2 = $"Hello {name.ToUpper()} you are {age} years old.";
+            greeting = string.Format("\tHello {0} you are {1} years old.", name.ToUpper(), age);
+            greeting2 = $"\tHello {name.ToUpper()} you are {age} years old.";
+            Console.WriteLine(greeting);
+            Console.WriteLine(greeting2);
+        }
+
+        #endregion
+        #region  Data Type Conversions
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static void NarrowingAttempt()
+        {
+            Console.WriteLine("***** Fun with type conversions *****");
+            short numb1 = 30000, numb2 = 30000;
+            // Explicitly cast the int into a short (and allow loss of data).
+            short answer = (short)Add(numb1, numb2);
+            Console.WriteLine("{0} + {1} = {2}",
+            numb1, numb2, answer);
+            Console.ReadLine();
+        }
+        static void ProcessBytes()
+        {
+            byte b1 = 100;
+            byte b2 = 250;
+            // This time, tell the compiler to add CIL code
+            // to throw an exception if overflow/underflow
+            // takes place.
+            try
+            {
+                byte sum = checked((byte)Add(b1, b2));
+                Console.WriteLine("sum = {0}", sum);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            ProcessBytes();
+            Console.ReadLine();
+
+        }
+    }
+}
