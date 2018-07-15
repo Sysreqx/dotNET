b9e3189390df7b57c242e502ba679cd11ae99f00
sysreqx <sysreqx@gmail.com>
2018-07-15 18:45:27 +0600

    hw

---
+++ itstep/HW/12.07.2018/Pro CSharp 7 Chapter3.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro_CSharp_7_Chapter3
+{
+    class Program
+    {
+        private static void ExecuteIfElseUsingConditionalOperator()
+        {
+            string stringData = "My textual data";
+            Console.WriteLine(stringData.Length > 0
+            ? "string is greater than 0 characters"
+            : "string is not greater than 0 characters");
+            Console.WriteLine();
+        }// Switch on a numerical value.
+        static void SwitchExample()
+        {
+            Console.WriteLine("1 [C#], 2 [VB]");
+            Console.Write("Please pick your language preference: ");
+            string langChoice = Console.ReadLine();
+            int n = int.Parse(langChoice);
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Good choice, C# is a fine language.");
+                    break;
+                case 2:
+                    Console.WriteLine("VB: OOP, multithreading, and more!");
+                    break;
+                default:
+                    Console.WriteLine("Well...good luck with that!");
+                    break;
+            }
+        }
+        static void SwitchOnStringExample()
+        {
+            Console.WriteLine("C# or VB");
+            Console.Write("Please pick your language preference: ");
+            string langChoice = Console.ReadLine();
+            switch (langChoice)
+            {
+                case "C#":
+                    Console.WriteLine("Good choice, C# is a fine language.");
+                    break;
+                case "VB":
+                    Console.WriteLine("VB: OOP, multithreading and more!");
+                    break;
+                default:
+                    Console.WriteLine("Well...good luck with that!");
+                    break;
+            }
+        }
+
+        static void SwitchOnEnumExample()
+        {
+            Console.Write("Enter your favorite day of the week: ");
+            DayOfWeek favDay;
+            try
+            {
+                favDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad input!");
+                return;
+            }
+            switch (favDay)
+            {
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Football!!");
+                    break;
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Another day, another dollar");
+                    break;
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("At least it is not Monday");
+                    break;
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("A fine day.");
+                    break;
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Almost Friday...");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Yes, Friday rules!");
+                    break;
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Great day indeed.");
+                    break;
+            }
+            Console.WriteLine();
+        }
+        public static void SwitchWithGoto()
+        {
+            var foo = 5;
+            switch (foo)
+            {
+                case 1:
+                    //do something
+                    goto case 2;
+                case 2:
+                    //do something else
+                    break;
+                case 3:
+                    //yet another action
+                    goto default;
+                default:
+                    //default action
+                    break;
+            }
+        }
+
+        static void ExecutePatternMatchingSwitch()
+        {
+            Console.WriteLine("1 [Integer (5)], 2 [String (\"Hi\")], 3 [Decimal (2.5)]");
+            Console.Write("Please choose an option: ");
+            string userChoice = Console.ReadLine();
+            object choice;
+            //This is a standard constant pattern switch statement to set up the example
+            switch (userChoice)
+            {
+                case "1":
+                    choice = 5;
+                    break;
+                case "2":
+                    choice = "Hi";
+                    break;
+                case "3":
+                    choice = 2.5;
+                    break;
+                default:
+                    choice = 5;
+                    break;
+            }
+            //This is new the pattern matching switch statement
+            //This is new the pattern matching switch statement
+            switch (choice)
+            {
+                case int i:
+                    Console.WriteLine("Your choice is an integer {0}.", i);
+                    break;
+                case string s:
+                    Console.WriteLine("Your choice is a string. {0}", s);
+                    break;
+                case decimal d:
+                    Console.WriteLine("Your choice is a decimal. {0}", d);
+                    break;
+                default:
+                    Console.WriteLine("Your choice is something else");
+                    break;
+            }
+        }
+
+        static void ExecutePatternMatchingSwitchWithWhen()
+        {
+            Console.WriteLine("1 [C#], 2 [VB]");
+            Console.Write("Please pick your language preference: ");
+            object langChoice = Console.ReadLine();
+            var choice = int.TryParse(langChoice.ToString(), out int c) ? c : langChoice;
+            switch (choice)
+            {
+                case int i when i == 2:
+                case string s when s.Equals("VB", StringComparison.OrdinalIgnoreCase):
+                    Console.WriteLine("VB: OOP, multithreading, and more!");
+                    break;
+                case int i when i == 1:
+                case string s when s.Equals("C#", StringComparison.OrdinalIgnoreCase):
+                    Console.WriteLine("Good choice, C# is a fine language.");
+                    break;
+                default:
+                    Console.WriteLine("Well...good luck with that!");
+                    break;
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            ExecutePatternMatchingSwitchWithWhen();
+            Console.Read();
+        }
+    }
+}
