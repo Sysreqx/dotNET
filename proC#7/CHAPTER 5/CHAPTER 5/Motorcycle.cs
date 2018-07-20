774943ce35741095a873c4b66f26874979915dc6
sysreqx <sysreqx@gmail.com>
2018-07-20 13:49:44 +0600

    Andrew Troelsen PRO C# 7.0

---
+++ proC#7/CHAPTER 5/CHAPTER 5/Motorcycle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHAPTER_5
+{
+    class Motorcycle
+    {
+        public string driverName;
+        public int driverIntensity;
+
+        // Constructor chaining.
+        public Motorcycle()
+        {
+            Console.WriteLine("In default ctor");
+        }
+        public Motorcycle(int intensity)
+        : this(intensity, "")
+        {
+            Console.WriteLine("In ctor taking an int");
+        }
+        public Motorcycle(string name)
+        : this(0, name)
+        {
+            Console.WriteLine("In ctor taking a string");
+        }
+        // This is the 'master' constructor that does all the real work.
+        public Motorcycle(int intensity, string name)
+        {
+            Console.WriteLine("In master ctor ");
+            if (intensity > 10)
+            {
+                intensity = 10;
+            }
+            driverIntensity = intensity;
+            driverName = name;
+        }
+
+        // New members to represent the name of the driver.
+        public void SetDriverName(string name)
+        {
+            this.driverName = name;
+        }
+
+        static void MakeSomeBikes()
+        {
+            // driverName = "", driverIntensity = 0
+            Motorcycle m1 = new Motorcycle();
+            Console.WriteLine("Name= {0}, Intensity= {1}",
+            m1.driverName, m1.driverIntensity);
+            // driverName = "Tiny", driverIntensity = 0
+            Motorcycle m2 = new Motorcycle(name: "Tiny");
+            Console.WriteLine("Name= {0}, Intensity= {1}",
+            m2.driverName, m2.driverIntensity);
+            // driverName = "", driverIntensity = 7
+            Motorcycle m3 = new Motorcycle(7);
+            Console.WriteLine("Name= {0}, Intensity= {1}",
+            m3.driverName, m3.driverIntensity);
+        }
+    }
+}
---
+++ proC#7/CHAPTER 5/CHAPTER 5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// The Role of the Default Constructor.
+
+namespace CHAPTER_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with class Types *****\n");
+            // Make a Motorcycle.
+            Motorcycle c = new Motorcycle(5);
+            c.SetDriverName("Tiny");
+            MakeSomeBikes();
+            Console.ReadLine();
+        }
+
+        static void MakeSomeBikes()
+        {
+            // driverName = "", driverIntensity = 0
+            Motorcycle m1 = new Motorcycle();
+            Console.WriteLine("Name= {0}, Intensity= {1}",
+            m1.driverName, m1.driverIntensity);
+            // driverName = "Tiny", driverIntensity = 0
+            Motorcycle m2 = new Motorcycle(name: "Tiny");
+            Console.WriteLine("Name= {0}, Intensity= {1}",
+            m2.driverName, m2.driverIntensity);
+            // driverName = "", driverIntensity = 7
+            Motorcycle m3 = new Motorcycle(7);
+            Console.WriteLine("Name= {0}, Intensity= {1}",
+            m3.driverName, m3.driverIntensity);
+        }
+    }
+}
